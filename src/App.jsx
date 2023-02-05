56a86e759e227387e77b7ba77870d56c452ba031
squinteye1 <neil.d.mckay@googlemail.com>
2023-02-05 22:24:14 +0000

    base site

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react'
-import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="app">
-      <h1>Nellie's Site</h1>
+
       <Navbar />
       <Header />
       <About />
       <Work />
       <Skills />
-      <Testimonials />
+      <Testimonial />
       <Footer />
     </div>
   )
